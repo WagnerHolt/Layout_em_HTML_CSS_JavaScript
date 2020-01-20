5acfcfaaa85c409872122819aa154f2679062fb3
Carlos Wagner <carlosconceicao-@hotmail.com>
2020-01-20 15:16:39 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true, //botões
+    infinite: true, //animação infinita
+    speed: 300, //velocidade
+    slidesToShow: 1,//quantidade de slides por vez
+    adaptiveHeight: true, //adaptar as alturas
+    autoplay: true, //autoplay
+    autoplaySpeed: 2000 //velocidades autoplay
+});
